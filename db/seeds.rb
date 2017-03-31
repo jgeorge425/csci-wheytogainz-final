236a2e225eb8faeffe7a9ca7998633a12d3031ad
Johnny George <jpgeorge@email.uscupstate.edu>
2017-03-31 18:47:03 +0000

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,18 @@ User.create!(name:  "Johnny George",
              email: "jpgeorge@email.uscupstate.edu",
              password:              "Jman4252210",
              password_confirmation: "Jman4252210",
-             admin: true)
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
