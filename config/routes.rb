562fa3e9ef82b21987b5ab91041aa5c4ed236016
Johnny George <jpgeorge@email.uscupstate.edu>
2017-03-31 00:23:00 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get  '/community',   to: 'static_pages#community'
   get  '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
-  get '/signup', to: 'user#new'
+  get '/signup', to: 'users#new'
+  resources :users
 end
